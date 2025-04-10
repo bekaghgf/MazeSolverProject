c07d851ae050e448c4002ac57cf8b046d88428be
Бексултан <beksultan.erkinbaev@alatoo.edu.kg>
2025-04-10 23:28:12 +0600

    added initialization and storage structure of the maze

--- MazeSolverProject.java
+++ MazeSolverProject.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
 public class MazeSolverProject {
+    private static final char WALL = '#';
+    private static final char PATH = ' ';
+    private static final char VISITED = '.';
+
+    private static char[][] maze;
+    private static boolean[][] visited;
+    private static int rows, cols;
+    private static int[] start, exit;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of rows (min. 10): ");
@@ -8,5 +17,10 @@ public class MazeSolverProject {
         System.out.print("Enter number of columns (min. 10): ");
         int cols = scanner.nextInt();
         System.out.println("Sizes: " + rows + " x " + cols);
+
     }
+    private static void generateMaze() {}
+    private static boolean findPath(int x, int y) { return false; }
+    private static void printMaze(boolean[][] visitedMap) {}
+
 }
